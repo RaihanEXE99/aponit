0a095bba45d3e01603a7d7b9eb953f94d7b209f5
RaihanEXE99 <brainsta@brainstation.com.bd>
2024-08-17 03:14:33 +0600

    init

---
+++ .nuxt/mdc-imports.mjs
@@ -0,0 +1,10 @@
+import _Highlight from 'C:/Users/Raihan/Documents/AUG/Aponit/nuxt-ui-pro-landing/node_modules/@nuxtjs/mdc/dist/runtime/highlighter/rehype-nuxt.js'
+
+export const remarkPlugins = {
+}
+
+export const rehypePlugins = {
+  'highlight': { instance: _Highlight, options: {} },
+}
+
+export const highlight = {"theme":{"light":"material-theme-lighter","default":"material-theme","dark":"material-theme-palenight"}}
